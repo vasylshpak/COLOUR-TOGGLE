7f9c1c7af5a7a50fd10bb2d7bf0308a95de93595
Vasyl Shpak <31392756+vasylshpak@users.noreply.github.com>
2018-12-05 22:23:34 +0100

    Add files via upload

---
+++ ColorToggle/toggle.js
@@ -0,0 +1,77 @@
+var button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+   document.body.style.background = "#444444";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var button = document.querySelector("button");
+// var isPurple = false;
+
+
+// button.addEventListener("click", function(){
+// 	if(isPurple){
+// 		document.body.style.background = "white";
+// 	} else {
+// 		document.body.style.background = "purple";
+// 	}
+// 	isPurple = !isPurple;
+// });
+
+
+button.addEventListener("click", function(){
+	document.body.classList.toggle("purple");
+});
+*/
